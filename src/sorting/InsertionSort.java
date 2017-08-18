b9152e3d007f1753ce7a817f1cf7ece832cc3150
Mani teja Kurapati <ManiTeja.Kurapati@gmail.com>
2017-08-18 16:07:16 -0500

    Initial commit-insertion, select, merge sorts

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package sorting;
+
+import java.util.Arrays;
+
+/**
+ * @author ManiTeja (MK051340)
+ * 
+ */
+public class InsertionSort {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+
+        int[] input = { 9, 8, 6, 99, 100, 7, 3, 1, 4, 2, 5, 55, 44, 33 };
+        int[] output = InsertionSort.perform(input);
+        System.out.println(Arrays.toString(output));
+
+    }
+
+    /**
+     * Sorting Procedure.
+     */
+    public static int[] perform(final int[] A) {
+
+        for (int i = 0; i < A.length; i++) {
+            int key = A[i];
+            int j = i - 1;
+            while (j >= 0 && A[j] > key) {
+                A[j + 1] = A[j];
+                j--;
+            }
+            A[j + 1] = key;
+        }
+        return A;
+    }
+}
---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(final String[] args) {
+
+        int[] input = { 9, 8, 6, 99, 100, 7, 3, 1, 4, 2, 5, 55, 44, 33 };
+        int[] output = SelectionSort.perform(input);
+        System.out.println(Arrays.toString(output));
+
+    }
+
+    /**
+     * Sorting Procedure.
+     */
+    public static int[] perform(final int[] inputArr) {
+        int arrLength = inputArr.length;
+        for (int i = 0; i <= arrLength - 2; i++) {
+            int smallest = i;
+            for (int j = i + 1; j <= arrLength - 1; j++) {
+                if (inputArr[j] < inputArr[smallest]) {
+                    smallest = j;
+                }
+            }
+            int temp = inputArr[i];
+            inputArr[i] = inputArr[smallest];
+            inputArr[smallest] = temp;
+        }
+        return inputArr;
+    }
+}
