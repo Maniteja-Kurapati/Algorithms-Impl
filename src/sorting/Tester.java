3606f2e5599d905eecde2c8b2373f4416c86dafd
Mani Teja <maniteja.kurapati@gmail.com>
2017-10-04 22:14:48 -0500

    Remove dupes

--- src/sorting/Tester.java
+++ src/sorting/Tester.java
@@ -469,4 +469,18 @@ public class Tester {
 		}
 		return sum;
 	}
+
+	private static int removeDuplicates(int[] nums) {
+		int pointer = 0;
+		int length = nums.length > 0 ? 1 : 0;
+
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				nums[pointer + 1] = nums[i];
+				pointer++;
+				length++;
+			}
+		}
+		return length;
+	}
 }
--- src/sorting/WhiteBoard.java
+++ src/sorting/WhiteBoard.java
@@ -6,8 +6,25 @@ public class WhiteBoard {
 
 		int[] input = new int[] { 1, 2, -2, -1 };
 
-		System.out.println();
+		System.out.println(WhiteBoard.strStr("mississippi", "issip"));
 
 	}
 
+	private static int strStr(String haystack, String needle) {
+		if (haystack == needle || haystack.isEmpty() || needle.isEmpty()) {
+			return (haystack.isEmpty() && needle.isEmpty()) ? 0 : haystack.isEmpty() ? -1 : 0;
+		}
+		int needlePointer = 0;
+		for (int i = 0; i < haystack.length(); i++) {
+			if (haystack.charAt(i) == needle.charAt(needlePointer)) {
+				needlePointer++;
+			} else {
+				needlePointer = 0;
+			}
+			if (needlePointer == needle.length()) {
+				return i - needle.length() + 1;
+			}
+		}
+		return -1;
+	}
 }
